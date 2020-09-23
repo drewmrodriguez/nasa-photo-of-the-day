a324c57f6653589d01c12330f826fac2cf6c8035
drewmrodriguez <drewmrodriguez@gmail.com>
2020-09-22 19:00:25 -0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import Header from "./Header";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState({});
+  useEffect(()=> {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=7Jgd7Ewd6gZ3UK0bg32Fsu4E2XuefbrUPMv03teN')
+    .then(response => {
+      setData(response.data);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  }, []);
+
   return (
     <div className="App">
+      <Header />
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = (props) => {
+    console.log("Header", props);
+    return (
+        <div className="header">NASA'S Photo of the Day</div>
+    );
+};
+
+export default Header;
